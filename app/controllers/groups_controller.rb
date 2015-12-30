54da827b6fe381493eaf49177c56a7c0f7f5ec7f
WILLIAM <will2b@gmail.com>
2015-12-30 23:42:34 +0800

    added update action in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -26,6 +26,15 @@ def edit
 	@group = Group.find(params[:id])
 end
 
+def update
+	@group = Group.find(params[:id])
+
+	if @group.update(group_params)
+		redirect_to groups_path, notice: "修改成功"
+	else
+		render :edit
+	end
+end
 
 private
 
