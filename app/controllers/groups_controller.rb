96869ff502107fc088af9863a626aa7e0bb8b79d
WILLIAM <will2b@gmail.com>
2015-12-30 23:29:24 +0800

    added CRUD in groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,6 +4,29 @@ def index
 	@groups = Group.all
 end
 
+def new
+	@group = Group.new
+end
 
+def create
+	@group = Group.create(group_params)
+
+	if @group.save
+		redirect_to groups_path
+	else
+		render :new
+	end
+end
+
+def show
+	@group = Group.find(params[:id])
+end
+
+
+private
+
+def group_params
+	params.require(:group).permit(:title, :description)
+end
 
 end
