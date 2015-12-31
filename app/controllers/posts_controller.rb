c42b37674c1f7294459fed850ff6852cc3641b12
WILLIAM <will2b@gmail.com>
2016-01-01 00:18:24 +0800

    added post action new & create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
 class PostsController < ApplicationController
+
+	def new
+		@group = Group.find(params[:group_id])
+		@post = @group.posts.new
+	end
+
+	def create
+		@group = Group.find(params[:group_id])
+		@post = @group.posts.build(post_params)
+
+		if @post.save
+			redirect_to group_path(@group), notice: "post has been successfully created"
+		else
+			render :new
+		end
+	end
+
+private
+
+def post_params
+	params.require(:post).permit(:content)
+	
+end
+
+
+
+
 end
